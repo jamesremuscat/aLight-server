5ee3b1e3b01c81c564edd8835ddd198b404226ce
James Muscat <jamesremuscat@gmail.com>
2015-10-17 10:13:55 +0100

    Receive messages, and dump them to stdout.

--- src/alight/server.py
+++ src/alight/server.py
@@ -1,8 +1,20 @@
-class Server:
-    @staticmethod
-    def run():
-        pass
+import socket
+from threading import Thread
+
+
+class Server(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(('', 8899))
+
+    def run(self):
+        while True:
+            data, addr = self.socket.recvfrom(64)  # buffer size is 64 bytes
+            if data is not None:
+                print data, addr
 
 
 if __name__ == "__main__":
-    Server.run()
+    Server().start()
