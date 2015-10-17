61180368629b1c85699f6791a5df1d8fc9e4314e
James Muscat <jamesremuscat@gmail.com>
2015-10-17 21:15:50 +0100

    Also test turning zones off.

--- src/alight/test/testUpdateModel.py
+++ src/alight/test/testUpdateModel.py
@@ -1,6 +1,6 @@
 import unittest
 from alight.state import Zones
-from alight.commands import Group1OnCommand, Group2OnCommand, Group3OnCommand, Group4OnCommand
+from alight.commands import Group1OnCommand, Group2OnCommand, Group3OnCommand, Group4OnCommand, Group1OffCommand, Group2OffCommand, Group3OffCommand, Group4OffCommand
 
 
 class TestUpdateModel(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestUpdateModel(unittest.TestCase):
         unittest.TestCase.setUp(self)
         self.model = Zones()
 
-    def testTurnZoneOn(self):
+    def testTurnZoneOnAndOff(self):
         cmd = Group1OnCommand()
         self.model.handleCommand(cmd)
 
@@ -29,6 +29,31 @@ class TestUpdateModel(unittest.TestCase):
         self.assertEqual(3, self.model.activeZone)
 
         cmd = Group4OnCommand()
+        self.model.handleCommand(cmd)
+
+        self.assertTrue(self.model.zones[4].on)
+        self.assertEqual(4, self.model.activeZone)
+
+        # And off again
+        cmd = Group1OffCommand()
+        self.model.handleCommand(cmd)
+
+        self.assertTrue(self.model.zones[1].on)
+        self.assertEqual(1, self.model.activeZone)
+
+        cmd = Group2OffCommand()
+        self.model.handleCommand(cmd)
+
+        self.assertTrue(self.model.zones[2].on)
+        self.assertEqual(2, self.model.activeZone)
+
+        cmd = Group3OffCommand()
+        self.model.handleCommand(cmd)
+
+        self.assertTrue(self.model.zones[3].on)
+        self.assertEqual(3, self.model.activeZone)
+
+        cmd = Group4OffCommand()
         self.model.handleCommand(cmd)
 
         self.assertTrue(self.model.zones[4].on)
