bae81b7a15f68ee4528c744cde7af547d22e164f
James Muscat <jamesremuscat@gmail.com>
2015-10-17 12:38:06 +0100

    Implement a basic relay that just passes packets on to another IP.

--- src/alight/server.py
+++ src/alight/server.py
@@ -5,27 +5,52 @@ from threading import Thread
 from time import sleep
 
 
-logger = logging.getLogger("Server")
-
-
 class Server(Thread):
+    logger = logging.getLogger("Server")
 
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(('', 8899))
+        self.listeners = []
+
+    def addListener(self, listener):
+        self.listeners.append(listener)
 
     def run(self):
         while True:
             data = self.socket.recv(64)  # buffer size is 64 bytes
             if data is not None:
                 cmd = parseCommand(data)
-                logger.debug("Received {0}".format(cmd))
+                self.logger.debug("Received {1} => {0}".format(cmd, data))
+                for listener in self.listeners:
+                    listener.handleCommand(cmd)
+
+
+def byteArrayToString(byteArray):
+    return ''.join(chr(b) for b in byteArray)
+
+
+class Relay(object):
+    logger = logging.getLogger("Relay")
+
+    def __init__(self, destinationIP, destinationPort=8899):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.destinationIP = destinationIP
+        self.destinationPort = destinationPort
+
+    def handleCommand(self, command):
+        string = byteArrayToString(command.getBytes())
+        self.logger.debug("Relaying message {2} to {0}:{1}".format(self.destinationIP, self.destinationPort, string))
+        self.socket.sendto(string, (self.destinationIP, self.destinationPort))
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    Server().start()
+    s = Server()
+    r = Relay("192.168.2.99")
+    s.addListener(r)
+    s.start()
     while True:
         sleep(1)
