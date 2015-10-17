6aa21182d61a27c11952a41d34c513950295e728
James Muscat <jamesremuscat@gmail.com>
2015-10-17 12:11:37 +0100

    Allow Ctrl+C to kill the server.

--- src/alight/server.py
+++ src/alight/server.py
@@ -2,6 +2,7 @@ import logging
 import socket
 from commands import parseCommand
 from threading import Thread
+from time import sleep
 
 
 logger = logging.getLogger("Server")
@@ -11,6 +12,7 @@ class Server(Thread):
 
     def __init__(self):
         Thread.__init__(self)
+        self.daemon = True
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(('', 8899))
 
@@ -25,3 +27,5 @@ class Server(Thread):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     Server().start()
+    while True:
+        sleep(1)
