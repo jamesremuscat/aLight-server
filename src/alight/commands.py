05d075f401720de36e4a79b136a1f96713018353
James Muscat <jamesremuscat@gmail.com>
2015-10-17 11:07:44 +0100

    Parse the three command bytes earlier.

--- src/alight/commands.py
+++ src/alight/commands.py
@@ -1,15 +1,22 @@
 def parseCommand(cmd):
     if len(cmd) != 3:
         raise InvalidCommandException(cmd)
-    return Command(cmd)
+    head = ord(cmd[0])
+    param = ord(cmd[1])
+    suffix = ord(cmd[2])  # always = 0x55
+    if suffix != 0x55:
+        raise InvalidCommandException(cmd)
+    return Command(head, param, suffix)
 
 
 class Command(object):
-    def __init__(self, bytedata):
-        self.bytedata = bytedata
+    def __init__(self, head, param, suffix):
+        self.head = head
+        self.param = param
+        self.suffix = suffix
 
     def __repr__(self):
-        return "{0:=#04x} {1:=#04x} {2:=#04x}".format(ord(self.bytedata[0]), ord(self.bytedata[1]), ord(self.bytedata[2]))
+        return "{0:=#04x} {1:=#04x} {2:=#04x}".format(self.head, self.param, self.suffix)
 
 
 class InvalidCommandException(Exception):
